35e97e5da1ec43d13a26a128e2819b699ac3bfb0
Gabriel Berto <gabriel@codengage.com>
2018-01-17 23:40:39 -0200

    Alterações

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,41 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  validate1 = false;
+  validate2 = false;
+  validate3 = false;
+  barValidate = 0;
+
+  constructor() { }
+
+  validatesenha(senha: any) {
+    if (senha.length >= 6) {
+      this.validate1 = true;
+    } else {
+
+      this.validate1 = false;
+    }
+
+    var letras_maiusculas = "ABCDEFGHYJKLMNOPQRSTUVWXYZ";
+
+    if (senha.match(/[A-Z]/)) {
+      this.validate2 = true;
+    } else {
+      this.validate2 = false;
+    }
+
+
+    var numeros = '0123456789'
+
+    if (senha.match(/[0-9]/)) {
+      this.validate3 = true;
+    } else {
+      this.validate3 = false;
+    }
+
+    console.log(senha);
+  }
+
 }
+
